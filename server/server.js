939ac6f941cb15303b67f08baf6f557a9196beb0
Haris1994 <billichuha@hotmail.com>
2019-03-07 19:35:29 +0500

    Changes

--- server/server.js
+++ server/server.js
@@ -8,10 +8,10 @@ const app = express();
 
 app.use(express.static(publicPath));
 
-var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+//var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
 //var server_host = process.env.YOUR_HOST || '0.0.0.0';
-app.listen(port, function() {
-    console.log('Listening on port %d', server_port);
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
 });
 
 // app.listen(port, () => {
