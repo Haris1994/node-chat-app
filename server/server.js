32de6e4a984cd0ae52f55c589e640fe1eec1eccf
Haris1994 <billichuha@hotmail.com>
2019-03-07 19:29:14 +0500

    Changes

--- server/server.js
+++ server/server.js
@@ -2,11 +2,18 @@ const path = require('path');
 const express = require('express');
 
 const publicPath = path.join(__dirname , '../public');
+// const port = process.env.PORT || 3000;
 
 const app = express();
 
 app.use(express.static(publicPath));
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000');
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+app.listen(server_port, server_host, function() {
+    console.log('Listening on port %d', server_port);
 });
+
+// app.listen(port, () => {
+//     console.log(`Server is up on port ${port}`);
+// });
